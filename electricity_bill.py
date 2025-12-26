4e6d8d4559b01402cca670d0960e324710130001
manojchikkumbi <chikkumbir@gmail.com>
2025-12-26 20:49:40 +0530

    Update electricity_bill.py

--- electricity_bill.py
+++ electricity_bill.py
@@ -1,1 +1,11 @@
-pytest
+from electricity import electricity_bill
+
+def test_electricity_bill():
+    expected_output = (
+        "Customer Name: Rahul\n"
+        "Units Consumed: 120\n"
+        "Rate per Unit: 5\n"
+        "Total Bill Amount: 600"
+    )
+
+    assert electricity_bill("Rahul", 120, 5) == expected_output
